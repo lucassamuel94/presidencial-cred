57e6f86cfee36a835f19f4c515fbb61bc84d3fef
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-02-06 08:06:02 -0300

    fix accessibility FAB WhatsApp

--- src/components/ui/fab.tsx
+++ src/components/ui/fab.tsx
@@ -7,6 +7,7 @@ export default function FAB() {
       target='_blank'
       rel='noopener noreferrer'
       className='fixed bottom-10 right-6 z-50 animate-bounce rounded-full bg-green p-5 text-gray-900 shadow-2xl lg:bottom-20 lg:right-20'>
+      <span className='sr-only'>Falar no WhatsApp</span>
       <WhatsappLogo size={48} />
     </a>
   )
