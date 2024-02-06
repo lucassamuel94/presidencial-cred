213ac14733bc11bd0cf99db2c3ee09e066a76038
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-02-05 21:53:05 -0300

    update style component Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,31 +1,26 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex w-full md:w-max items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-neutral-900 text-neutral-50 hover:bg-neutral-900/90',
-        destructive: 'bg-red-500 text-neutral-50 hover:bg-red-500/90',
-        outline:
-          'border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900',
+        primary: 'bg-brand-400 text-gray-900 hover:bg-brand-500',
+        whatsapp: 'bg-green text-gray-900 hover:bg-green/90',
         secondary: 'bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80',
         ghost: 'hover:bg-neutral-100 hover:text-neutral-900',
         link: 'text-neutral-900 underline-offset-4 hover:underline'
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10'
+        default: 'px-6 py-3'
       }
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'whatsapp',
       size: 'default'
     }
   }
