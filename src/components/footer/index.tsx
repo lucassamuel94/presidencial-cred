4ca3e8e440f77049f69fd8d6111535e07dcf39ed
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-02-06 08:34:46 -0300

    update align items with component Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer>
-      <div className='container flex flex-col items-center justify-between gap-4 border-t border-gray-700 py-16 text-gray-400 sm:flex-row'>
+      <div className='container flex flex-col items-start justify-between gap-4 border-t border-gray-700 py-16 text-gray-400 sm:flex-row lg:items-center'>
         <Link href={'/'} className='text-xl font-bold uppercase'>
-          Presidencial Prev
+          Presidencial Cred
         </Link>
 
         <span className='text-sm'>Todos os direitos reservados</span>
