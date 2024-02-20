278a805d70f84195a536ecb3d407c7314a7f1d83
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-02-20 14:46:15 -0300

    include facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils'
 import { fontHeading, fontSans } from '@/styles/fonts'
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 type RootLayoutProps = {
   children: React.ReactNode
@@ -17,6 +18,38 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang='pt-br' className='scroll-smooth' suppressHydrationWarning>
+      <meta
+        name='facebook-domain-verification'
+        content='tvjpotttjnevme84fevx1ub9ek7itk'
+      />
+      <Script
+        id='fb-pixel'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '884566266684252');
+          fbq('track', 'PageView');
+          `
+        }}
+      />
+
+      <noscript>
+        <img
+          height='1'
+          width='1'
+          style={{ display: 'none' }}
+          src='https://www.facebook.com/tr?id=884566266684252&ev=PageView&noscript=1'
+        />
+      </noscript>
+
       <body
         className={cn(
           fontSans.variable,
