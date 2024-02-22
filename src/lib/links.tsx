2b98f532938b2a81fb70e2558c5c0cfcc5bcb827
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-02-22 14:40:00 -0300

    update link WhatsApp

--- src/lib/links.tsx
+++ src/lib/links.tsx
@@ -1,2 +1,2 @@
-export const linkWhatsApp = 'https://api.whatsapp.com/send?phone=5584998970057'
+export const linkWhatsApp = 'https://api.whatsapp.com/send?phone=5584994645030'
 export const linkInstagram = 'https://www.instagram.com/isapresidencial/'
